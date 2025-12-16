7c55646f78982ab70c010cdde720a07c5f824031
Laiba Khan <codewithlaiba28@gmail.com>
2025-12-16 21:38:37 +0500

    add

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import About1 from '../components/About1'
+import Features from '../components/Features/Features'
+import GetinTouch from '../components/Contact/GetinTouch'
+import Contact from '../components/Contact/Contact'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero tittle="Manage Your Tasks" subtittle="Like a Pro" text="A simple, fast, and beautiful todo manager built with modern technology to help you stay organized and productive." />
+      <About1 />
+      <Features />
+      <GetinTouch />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
