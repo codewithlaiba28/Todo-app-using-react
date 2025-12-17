72029c4b083f0f709ee91eb311bf29ab4099352b
Laiba Khan <codewithlaiba28@gmail.com>
2025-12-17 21:37:38 +0500

    add

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,6 +4,7 @@ import "../Todo.css";
 const TodoApp = () => {
     const [todolist, setTodolist] = useState([])
 
+
     let saveTodoList = (e) => {
         let todoname = e.target.todoname.value;
         if (!todolist.includes(todoname)) {
@@ -14,19 +15,30 @@ const TodoApp = () => {
             alert("Todo already exists or is invalid!");
         }
         e.preventDefault();
+        e.target.todoname.value = "";
     }
+    let list = todolist.map((value, index) => {
+        return (
+            <ToDolistItems value={value} key={index} indexNumber={index}
+                todolist={todolist}
+                setTodolist={setTodolist}
+            />
+        )
+    })
     return (
-        <div>
-            <h1>Hi</h1>
-            <form className='form' onSubmit={saveTodoList}>
-                <input className='input' type="text" name='todoname' />
-                <button className='button'>Save</button>
-            </form>
+        <div className='main-div'>
+            <div className='todo-app'>
+                <h1>TODO APP</h1>
+                <form className='form' onSubmit={saveTodoList}>
+                    <input className='input' type="text" name='todoname' />
+                    <button className='button'>Save</button>
+                </form>
 
-            <div className="outerdiv">
-                <ul>
-                    <ToDolistItems />
-                </ul>
+                <div className="outerdiv">
+                    <ul>
+                        {list}
+                    </ul>
+                </div>
             </div>
         </div>
     )
@@ -34,8 +46,17 @@ const TodoApp = () => {
 
 export default TodoApp
 
-function ToDolistItems() {
+function ToDolistItems({ value, indexNumber, todolist, setTodolist }) {
+    let [status, setStatus] = useState(false)
+    let deleteRow = () => {
+        let finalData = todolist.filter((val, idx) => idx != indexNumber);
+        setTodolist(finalData);
+    }
+    let checkStatus = () => {
+        setStatus(!status);
+
+    }
     return (
-        <li>HTML <span>&times;</span></li>
+        <li className={(status) ? "completetodo" : ''} onClick={checkStatus}> {indexNumber + 1} {value} <span onClick={deleteRow}>&times;</span></li>
     )
 }
